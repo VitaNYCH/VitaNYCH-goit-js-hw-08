4b39cebdb8f0414ee0aaa0e21c0108db4df3fb06
VitaNYCH <vitanychvoloda@yahoo.com>
2023-03-01 14:55:20 +0200

    Added library to task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,50 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const picturesContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
 
+picturesContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+picturesContainer.addEventListener('click', onPicturesContainerClick);
+
+function createGalleryItemsMarkup(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+function onPicturesContainerClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  console.log(event.target.dataset.source);
+
+  const instance = basicLightbox.create(`
+    <img src='${event.target.dataset.source}' width="800" height="600">
+`);
+
+  instance.show();
+
+  picturesContainer.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
+  picturesContainer.removeEventListener('keydown', onPicturesContainerClick);
+}
 console.log(galleryItems);
